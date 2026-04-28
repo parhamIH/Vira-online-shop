9bb95c41cacbca0cd532e2e55a68c9c236c27893
parham <parham.ilaghi@gmail.com>
2026-04-28 22:11:04 +0330

    finaled

--- main/settings.py
+++ main/settings.py
@@ -110,10 +110,15 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'shahan_shop1',
+        'USER': 'root',
+        'PASSWORD': 'mysql',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
